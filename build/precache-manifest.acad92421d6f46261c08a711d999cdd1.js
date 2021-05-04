df8770b4e4df6a63933b551b185a62993ded4f41
Robert <orazurobert@yahoo.com>
2021-05-04 12:03:13 +0100

    push

---
+++ build/precache-manifest.acad92421d6f46261c08a711d999cdd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea26fb6fd6c53f24a80ce95b49fa76d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "984ba0f029a2b7b95d08",
+    "url": "/static/css/main.c2f777cd.chunk.css"
+  },
+  {
+    "revision": "e08d779f62b8c66ab55f",
+    "url": "/static/js/2.34892894.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.34892894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984ba0f029a2b7b95d08",
+    "url": "/static/js/main.47469740.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,54 @@
+// WEBSOCKET CONNECTION
+const WebSocketServer = require("ws").Server;
+const wss = new WebSocketServer({ port: 8080 });
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3001
+const cors = require('cors')
+
+console.log('web socket')
+// THIS VARIABLE STORES EVERY CONNECTION AS A DIFFERENTLY CLIENT
+//SO THAT THE SAME ACTION WILL BE PERFORMED ON ALL OF THEM
+let clients = [];
+
+// FUNCTION TO PUSH EVERY NEW CONNECTION INTO THE ARRAY AS A CLIENT
+wss.on("connection", (connection) => {
+    clients.push(connection);
+    wss.broadcast({ username: "admin", message: "A User Has Joined The Chat" });
+
+    // EVERY MESSAGE RECIEVED BY THE SERVER IS PUBLISHED TO ALL CLIENTS
+    connection.on("message", (message) => {
+        const data = JSON.parse(message);
+        wss.broadcast(data, connection);
+    });
+});
+
+setInterval(cleanUp, 100);
+
+wss.broadcast = function (data, sender) {
+    clients.forEach(function (client) {
+        if (client !== sender) {
+            client.send(JSON.stringify(data))
+        }
+    })
+}
+
+function cleanUp() {
+    const clientsLeaving = clients.filter(
+        (client) => client.readyState === client.CLOSED
+    );
+    clients = clients.filter((client) => client.readyState !== client.CLOSED);
+    clientsLeaving.forEach((client) =>
+        wss.broadcast({ username: "admin", message: "A User Has Left The Chat" }, client)
+    );
+}
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+  })
